100f2e84751a240595878f5029b92035fe7deb16
suleiman-duwane <suleimanduwane90@gmail.com>
2023-12-01 12:55:41 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+document.getElementById("main").remove()
+
+const newHeader = document.createElement("h1")
+
+newHeader.id = "victory"
+
+newHeader.textContent = "Omar is the champion"
